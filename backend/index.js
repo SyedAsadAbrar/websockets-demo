a37e6b16a3ae1933444cb7a782c0a168790263c7
Asad <syedasadabrar@gmail.com>
2023-08-07 18:55:29 +0500

    fixed closing connection message

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ wss.on("connection", (ws, req) => {
     wss.clients.forEach((client) => {
       if (ws !== client && client.readyState === WebSocket.OPEN) {
         client.send(
-          JSON.stringify({ clientId: client.id, message: "has left the chat" })
+          JSON.stringify({ clientId: ws.id, message: "has left the chat" })
         );
       }
     });
